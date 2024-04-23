49f2e1a1b966fee2fed3224b939836a922b442e7
cbgaggi <chrestinei02@gmail.com>
2024-04-22 17:19:14 -0700

    submission for VU meter
    first submission with video, updated code, and change log

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# 1 CHANGELOG
+
+## {4/22/2024}
+### Contributors
+Chrestine Gaggi
+
+### Added
+Video of VU meter operating
+Updated code in main.py file
+
+### Changed
+Nothing was changed only added
+
+### Removed
+...
--- main.py
+++ main.py
@@ -13,10 +13,20 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
+    board.IO33, # type: ignore
+    board.IO34, # type: ignore
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
     # do the rest...
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
+
+analog_vals = [6000, 7000, 8000, 9000, 10000, 20000, 30000, 40000, 50000, 60000, 70000]
 
 for led in leds:
     led.direction = Direction.OUTPUT
@@ -27,11 +37,17 @@ while True:
 
     print(volume)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    leds_initialize = 0
 
-    sleep(1)
+    for i, analog_vals in enumerate(analog_vals):
+        if volume >= analog_vals:  # Check if volume exceeds the threshold
+            leds_initialize = i + 1
 
+    # Turn on/off LEDs
+    for i, led in enumerate(leds):
+        led.value = i < leds_initialize
+
+    sleep(0.1)
     # instead of blinking,
     # how can you make the LEDs
     # turn on like a volume meter?
